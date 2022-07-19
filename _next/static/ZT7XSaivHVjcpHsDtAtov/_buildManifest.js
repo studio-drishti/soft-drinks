35cee409f82768bfbef8c8bb8e14c4b2d98c9245
jayarnielsen <jayarnielsen@gmail.com>
2022-07-19 21:49:37 +0000

    Deploying to gh-pages from @ studiodrishti/soft-drinks@56eb921c9873a41a82e8fe08bb7c88dd27533017 🚀

---
+++ _next/static/ZT7XSaivHVjcpHsDtAtov/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/490-f02b49c267d65129.js","static/chunks/pages/index-748fe3bd19a0a941.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
